8bb27cb424be599f35b6c9da94508a6790bc1aea
hamza <hk48709@gmail.com>
2023-05-31 15:17:36 +0500

    game ha yeah

---
+++ helloworld.js
@@ -0,0 +1,27 @@
+let nam="hamza";
+let age=23;
+let ismarried=false;
+
+let user={
+    nm:nam,
+    ag:age,
+    status:ismarried
+}
+let arr_of_user=[user,user]
+
+console.log("hello world",nam,age,ismarried,user);
+console.log(arr_of_user);
+console.log(!ismarried);
+// hamza
+//operation
+
+let num1=1;
+let num2=4;
+console.log("sum ",num1+num2);
+console.log("min ",num1-num2);
+console.log("div ",num1/num2);
+console.log("mul",num1*num2);
+console.log("post-increment ",num1++);
+console.log(num1);
+console.log("pre-incrment ",++num2);
+
